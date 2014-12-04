6bcc45443f3dd0abe7a66dd12d2616f7f6d66a10
Jana Riess <j.riess@wtnet.de>
2014-12-04 01:03:36 +0100

    changed color in gol

--- GameOfLife.hs
+++ GameOfLife.hs
@@ -4,7 +4,7 @@ import CellularAutomata2D
 import qualified Graphics.UI.SDL as SDL
 
 gol :: Space Int -> IO ()
-gol space = runCellularAutomata2D space [0,1] ([0, 2334554] !!)
+gol space = runCellularAutomata2D space [0,1] ([0, 42345458] !!)
     (makeTotalMoorRule [2,3] [3])
 
 glieder = initSpace 20 20 (zip [(0,2),(1,2),(2,2),(2,1),(1,0)] (repeat 1))
