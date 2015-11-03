5d3bfec210e5b9f4879e5be6e107e841e1a0df4a
Jana Riess <j.riess@wtnet.de>
2015-11-03 22:42:51 +0100

    docs

--- doc.sh
+++ doc.sh
@@ -1,13 +1,9 @@
-
+if [ ! -d doc ]; then
+    mkdir doc
+fi
 
 haddock CellularAutomata2D.hs -h -o doc
 haddock GUI.hs -h -o doc
 haddock Term.hs -h -o doc
-haddock Brain.hs -h -o doc
-haddock Forest.hs -h -o doc
-haddock GameOfLife.hs -h -o doc
-haddock NAND.hs -h -o doc
-haddock RockPaperScissors.hs -h -o doc
-haddock WireWorld.hs -h -o doc
 
 
