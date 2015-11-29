a691111a7de79be290a5404fc9c45fbd1ce1ef3d
Jana Riess <j.riess@wtnet.de>
2015-11-29 21:59:09 +0100

    voting rules from the cam book

---
+++ Voting.hs
@@ -0,0 +1,37 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Voting where
+
+import CellularAutomata2D
+import GUI
+
+main :: IO ()
+main = do
+    -- space <- randomSpace size [True, False]
+    -- runCellularAutomata2D space [True, False] (\c -> if c then white else black) anneal
+    space <- randomSpace size [0,1]
+    runCellularAutomata2D space [0,1] (\c -> if c == 1 then white else black) banks
+
+banks :: Rule Int
+banks = Rule neumannIndexDeltas $ \self neighbors ->
+    return $ case sum neighbors of
+        0 -> self
+        1 -> self
+        2 -> if neighbors !! 1 == neighbors !! 2 then self else 0
+        _ -> 1
+
+anneal :: Rule Bool
+anneal = Rule moorIndexDeltas $ \self neighbors ->
+    let count = length $ filter id $ self : neighbors
+    in return $ count >= 6 || count == 4
+
+majority :: Rule Bool
+majority = Rule moorIndexDeltas $ \self neighbors -> return $
+    length (filter id $ self : neighbors) > length (filter not $ self : neighbors)
+
+circle :: Torus Bool
+circle = initSpace size (\(row, col) -> 20 >=
+    sqrt (fromIntegral ((row - div (fst size) 2)^2 + (col - div (snd size) 2)^2)))
+
+size :: (Int, Int)
+size = (50, 50)
